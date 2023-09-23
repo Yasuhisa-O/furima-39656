82b65c494bd68e2d91c70b441f663421c30e9ac8
Yasuhisa-O <koriyama.o4m@gmail.com>
2023-09-23 15:35:36 +0900

    ログアウト状態の場合、出品ページに遷移しない処理を追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  before_action :move_to_login,  only: [:new]
+
   def index
   end
 
@@ -19,6 +21,10 @@ class ItemsController < ApplicationController
   private
   def item_params
     params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, :delivery_charge_id, :prefecture_id, :shipping_time_id, :price).merge(user_id: current_user.id)
+  end
+
+  def move_to_login
+    authenticate_user!
   end
 
 end
