8fbcb8b7d59998edceb5c2c0b8768b30d0f727c0
Yasuhisa-O <koriyama.o4m@gmail.com>
2023-09-24 17:54:18 +0900

    モデル単体のテストコード書く

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :item do
-    
+    name               {'test'}
+    description        {'test'}
+    category_id        {2}
+    condition_id       {2}
+    delivery_charge_id {2}
+    prefecture_id      {2}
+    shipping_time_id   {2}
+    price              {500}
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,78 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品出品機能' do
+
+    context '商品出品できないとき' do
+      it '商品名が必須であること' do
+        @item.name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
+      end
+
+      it '商品説明が必須であること' do
+        @item.description = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Description can't be blank")
+      end
+
+      it 'カテゴリ情報が必須であること' do
+        @item.category_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
+
+      it '商品の状態の情報が必須であること' do
+        @item.condition_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Condition can't be blank")
+      end
+
+      it '配送の負担の情報が必須であること' do
+        @item.delivery_charge_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery charge can't be blank")
+      end
+
+      it '発送元の地域の情報が必須であること' do
+        @item.prefecture_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture can't be blank")
+      end
+
+      it '発送までの日数の情報が必須であること' do
+        @item.shipping_time_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping time can't be blank")
+      end
+
+      it '価格の情報(数字入力)が必須であること' do
+        @item.price = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+
+      it '価格の情報(半角数字入力)が必須であること' do
+        @item.price = "１"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+
+      it '価格の情報で300円以下は出品できないこと' do
+        @item.price = 299
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be greater than 300")
+      end
+
+      it '価格の情報で10000000円以上は出品できないこと' do
+        @item.price = 10000000
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be less than 10000000")
+      end
+    end
+  end
 end
