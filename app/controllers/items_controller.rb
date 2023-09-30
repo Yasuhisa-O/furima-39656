9403f22fff4b2e68237fca8ba94687980212ac2b
Yasuhisa-O <koriyama.o4m@gmail.com>
2023-09-30 21:25:06 +0900

    商品編集ページへ遷移する機能を追加、ルーティングに制限をかける

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!,  only: [:new]
+  before_action :authenticate_user!,  only: [:new, :edit]
+  before_action :move_to_index, only: [:edit]
+  before_action :sold_edit, only: [:edit]
 
   def index
     @items = Item.all.order(created_at: :desc)
@@ -22,9 +24,37 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item.id)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
   def item_params
     params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, :delivery_charge_id, :prefecture_id, :shipping_time_id, :price).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    if user_signed_in?
+      not_user = Item.find(params[:id])
+      unless current_user == not_user.user
+        redirect_to action: :index
+      end
+    end
+  end
+
+  def sold_edit
+    if @item.order.present?
+      redirect_to root_path
+    end
   end
 
 end
